64b202703068d4e44d5e19d8eab2e6fdc0f1f333
11m245 <sivaraj2siva@gmail.com>
2023-01-03 14:53:05 +0530

    file systm task

---
+++ fileSystemTask.js
@@ -0,0 +1,17 @@
+import filesRouter from "./routes/files.route.js"
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log("app started in", PORT));
+app.use(express.json())
+
+app.get("/", function (rquest, response) {
+    response.send(`<p>Welcome to the Files API Task  </p>
+     <p>use files/create to create a new file</p>
+     <p>use files/read to create read the last created file</p>`
+    )
+})
+
+app.use("/files", filesRouter)
